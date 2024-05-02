e60e77491b2ab721cea8289d801e09723dc0ec07
Taran234 <ts406093@gmail.com>
2024-05-02 16:00:07 -0400

    rims

--- src/app/_components/usedrims.tsx
+++ src/app/_components/usedrims.tsx
@@ -13,35 +13,43 @@ interface Rims {
         };
     }[];
 };
+
 async function getData(): Promise<Rims[]> {
     const query = `*[_type == 'usedRims' ]{
         name,
         RimImages,
     }`
-    const data = await client.fetch<Rims[]>(query, {}, { cache: 'no-cache' });
-    return data;
+    try {
+        const data = await client.fetch<Rims[]>(query, {}, { cache: 'no-cache' });
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return [];
+    }
 }
-
 
 export async function UsedRims() {
     const data = await getData();
     console.log(data);
     return (
-        <div className="flex overflow-hidden relative flex-col justify-center text-white bg-zinc-900 items-center  py-10 min-h-screen">
+        <div className="flex overflow-hidden relative flex-col justify-center text-white bg-zinc-900 items-center pt-3 py-5 min-h-screen">
             {data.map((rim, index) => (
                 <div key={index} className="flex relative flex-col mt-3 w-full ">
-                    <div className="self-center text-4xl font-extrabold leading-10 py-10 text-center uppercase">
+                    <div className="self-center text-4xl font-extrabold leading-10 py-5 text-center uppercase">
                         {rim.name}
                     </div>
                     <div className="flex  justify-center items-center mt-10">
                         <div className="grid grid-cols-1 gap-5 md:gap-20 md:grid-cols-2 lg:grid-cols-3 px-5 md:px-10 self-stretch my-auto">
                             {rim.RimImages.map((rimImage, index) => (
-                                <img
-                                    key={index}
-                                    loading="lazy"
-                                    src={urlForImage(rimImage)} width={800} height={500} alt="Image"
-                                    className="mt-3 w-full shadow-sm aspect-[1.16]"
-                                />
+                                // Check if rimImage is properly formatted before rendering
+                                rimImage && rimImage.asset && rimImage.asset._ref ? (
+                                    <img
+                                        key={index}
+                                        loading="lazy"
+                                        src={urlForImage((rimImage))}
+                                        className="mt-3 w-full shadow-sm aspect-[1.16]"
+                                    />
+                                ) : null
                             ))}
                         </div>
                     </div>
@@ -50,5 +58,3 @@ export async function UsedRims() {
         </div>
     );
 }
-
-
