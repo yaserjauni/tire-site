67ccb6c3d59746f156375c9fe9900ea173582302
Taran234 <ts406093@gmail.com>
2024-05-05 15:06:30 -0400

    bg-fix

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -7,7 +7,7 @@ export async function Intro() {
   const imageUrls = await getDisplay();
   return (
     <section >
-      <div className="pt-10 text-black w-full gap-20  flex-col justify-center  bg-[#040d12] items-center">
+      <div className="pt-10 text-black w-full gap-20  flex-col justify-center  bg-gray-900 items-center">
         <SlideCard imageUrls={imageUrls} />
         <Search />
       </div>
