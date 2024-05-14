4c42ed51646ee067e0fd72ef57b4e7de0c1d9a07
Taran234 <ts406093@gmail.com>
2024-05-14 16:19:05 -0400

    d

--- src/app/product/[parameter]/page.tsx
+++ src/app/product/[parameter]/page.tsx
@@ -15,16 +15,20 @@ import { Products } from "@/app/_components/homePage";
 // }
 
 async function getFilteredData({ season, width, profile, wheelSize }: { season: string, width: string, profile: string, wheelSize: string }): Promise<Products[]> {
-    let categoryCondition = 'true';
+    let categoryCondition = '';
     if (season) {
         categoryCondition = `tireType == '${season}'`;
+    } else {
+        categoryCondition = `true`;
     }
-    let searchCondition = 'true';
-    if (width && profile && wheelSize) {
-        let search1 = `${width}/${profile}/${wheelSize}`;
-        let search2 = `${width}/${profile}R${wheelSize}`;
-        console.log(search1, search2, season || "all");
+    let searchCondition = '';
+    if (width) {
+        const search1 = `${width}/${profile}/${wheelSize}`;
+        const search2 = `${width}/${profile}R${wheelSize}`;
+        console.log(search1, search2);
         searchCondition = `name match '${search2}' || name match '${search1}' || spec match '${search1}' || spec match '${search2}'`;
+    } else {
+        searchCondition = `true`;
     }
 
     const query = `*[${categoryCondition} && ${searchCondition}] {
@@ -37,7 +41,7 @@ async function getFilteredData({ season, width, profile, wheelSize }: { season: 
         tireType,
         productImage,
     }`;
-    const data = await client.fetch<Products[]>(query, {}, { cache: 'no-cache' });
+    const data = await client.fetch<Products[]>(query, {}, { cache: 'no-store' });
     return data;
 }
 
