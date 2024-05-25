e3badcdb64ca4a6ccf5e12b6bc73576dfe5f07e4
Taran234 <ts406093@gmail.com>
2024-05-25 19:17:17 -0400

    map-footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import { BiLocationPlus } from "react-icons/bi";
+import { SlLocationPin } from "react-icons/sl";
 
 export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
-      <div className="flex flex-col justify-center bg-sky-900">
+      <div className="flex flex-col bg-sky-900">
         <div className="flex justify-center items-center px-10 py-7 w-full text-sm font-bold leading-5 text-center text-white uppercase bg-violet-900 max-md:px-5 max-md:max-w-full">
           <div className="flex gap-5 max-w-full justify-center items-center max-md:flex-wrap">
             {[
@@ -21,32 +23,47 @@ export function Footer() {
             ))}
           </div>
         </div>
-        <div className="flex justify-center items-center px-4 py-9 w-full bg-stone-950">
-          <div className="max-w-full">
-            <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-              <div className="flex flex-col w-[23%] max-md:ml-0 max-md:w-full">
-                <ContactInfo />
+        <div className="flex flex-col md:flex-row justify-center px-2 py-6 w-full bg-stone-950">
+          <div className="w-full md:w-1/3 flex flex-col items-center pr-10 mb-4 md:mb-0">
+            <ContactInfo />
+          </div>
+          <div className="w-full md:w-1/3 flex items-center justify-center mb-4 md:mb-0">
+            <Link href="https://maps.app.goo.gl/vT1nLMd8dNB8pHvY7" className="w-full h-full">
+              <Image
+
+                width={800}
+                height={200}
+                alt="Tire Empire"
+                src="/smallMap.png"
+                className="bg-cover w-full h-auto"
+              />
+            </Link>
+          </div>
+
+          <div className="w-full md:w-1/3 flex md:flex-col flex-col-reverse mr-5 justify-start ">
+            <Link href="/" className="flex flex-col items-center pt-5">
+
+              <div className="flex flex-row ">
+                <Image
+                  loading="lazy"
+                  width={55} // Provide specific width and height for next/image
+                  height={55}
+                  alt="Tire Empire"
+                  src="/tire.svg"
+                  className="aspect-square"
+                />
+                <span className="text-4xl text-[#05fd00] uppercase leading-[111%] font-semibold mt-2">Empire Tire</span>
               </div>
-              <div className="flex flex-col ml-5  justify-center item-center text-left w-[55%] max-md:ml-0 max-md:w-full">
-                <div className="mt-3.5 mx-5 text-l font-semibold leading-5 text-white max-md:mt-10 max-md:max-w-full">
-                  Tire Empire offers a range of services tailored to your needs, from tire sales to expert advice. Whether you're a dealership, wholesaler, or individual, they provide convenient options to meet your requirements. Contact Tire Empire for a hassle-free experience and to learn more about their services.
-                </div>
-              </div>
-              <div className="flex justify-center md:justify-start item-center md:items-start w-[30%] max-md:ml-0 max-md:w-full">
-                <Link href="/" className="flex items-start pt-10 space-x-2">
-                  <Image
-                    loading="lazy"
-                    width={55} // Provide specific width and height for next/image
-                    height={55}
-                    alt="Tire Empire"
-                    src="/tire.svg"
-                    className="aspect-square"
-                  />
-                  <span className="text-4xl text-[#05fd00] uppercase leading-[111%] font-semibold mt-2">Empire Tire</span>
-                </Link>
+            </Link>
+            <div className="flex flex-row md:pt-10 pt-5 items-left md:justify-center ">
+              <SlLocationPin size={25} color="white" />
+              <div className="flex flex-col text-lg space-y-2 font-semibold ml-2 leading-5 text-white " >
+                <div >  63 Courtland Ave E Unit C</div>
+                <div> Kitchener, ON N2G </div><div> 2T6, Canada</div>
               </div>
             </div>
           </div>
+
         </div>
       </div>
     </footer>
@@ -54,7 +71,7 @@ export function Footer() {
 }
 
 const ContactInfo = () => (
-  <div className="flex flex-col text-sm font-semibold leading-5 text-white">
+  <div className="flex flex-col md:text-lg text-base justify-center space-y-4 font-semibold leading-5 text-white">
     <ContactDetail iconSrc="/phone.svg" detail="1-519-827-7854" />
     <ContactDetail iconSrc="/mail.svg" detail="Tireempirekitchener@gmail.com" />
     <ContactDetail iconSrc="/cal.svg" detail="Hours: Mon-Fri 11:00 AM - 5:00 PM," />
@@ -69,7 +86,7 @@ interface ContactDetailProps {
 
 const ContactDetail = ({ iconSrc, detail }: ContactDetailProps) => (
   <div className="flex gap-1 mt-4">
-    <img loading="lazy" src={iconSrc} className="shrink-0 w-[11px] aspect-[0.79]" />
+    <img loading="lazy" src={iconSrc} className="shrink-0 md:w-[25px] sm:w-[11px] aspect-[0.79]" />
     <div className="flex-auto my-auto">{detail}</div>
   </div>
 );
